ab05290921dd37d0b54dc1f9a10bd45ba0ac17a8
kumoyunf <kumoyun.f@gmail.com>
2024-12-07 12:49:45 +0500

    Tiklangan o'zgarishlar

---
+++ lib/app_router.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:navoiy/models/author_model.dart';
+import 'package:navoiy/screens/author_screen.dart';
+import '../../models/data_model.dart';
+import '../../screens/chapter_screen.dart';
+import '../../screens/home_screen.dart';
+import 'screens/view_screen.dart';
+
+class OnGenerateRouter {
+  Route<dynamic>? router(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return navigateTo(const HomeScreen());
+      case '/chapter':
+        final title = (settings.arguments as List)[0];
+        final books = (settings.arguments as List)[1] as List<Book>;
+        return navigateTo(ChapterScreen(title: title, books: books));
+      case '/view':
+        final book = settings.arguments as Book;
+        return navigateTo(ViewScreen(book: book));
+      case '/authors':
+        final authors = settings.arguments as List<Author>;
+        return navigateTo(AuthorScreen(authors: authors));
+      default:
+        return navigateTo(Scaffold());
+    }
+  }
+
+  navigateTo(Widget widget) {
+    return MaterialPageRoute(builder: (ctx) => widget);
+  }
+}
